3dbceceff777841446ac1362dca4eb89d7b613f0
beepscore <support@beepscore.com>
2010-03-04 18:20:17 -0800

    Add UIView subclass ImageView.  In IB, change view to ImageView.  Add image file.

---
+++ Classes/ImageView.h
@@ -0,0 +1,16 @@
+//
+//  ImageView.h
+//  Image
+//
+//  Created by Steve Baker on 3/4/10.
+//  Copyright 2010 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ImageView : UIView {
+
+}
+
+@end
---
+++ Classes/ImageView.m
@@ -0,0 +1,33 @@
+//
+//  ImageView.m
+//  Image
+//
+//  Created by Steve Baker on 3/4/10.
+//  Copyright 2010 Beepscore LLC. All rights reserved.
+//
+
+#import "ImageView.h"
+
+
+@implementation ImageView
+
+
+- (id)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
